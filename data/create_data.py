0c46f64b4286d6bf2f990f721f5f50b82c4be4f5
EytanKats <eytan.kats@gmail.com>
2025-12-22 20:47:54 +0100

    crop patches using better fg mask, parallelize, use random zoom

--- data/create_data.py
+++ data/create_data.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append(".")
+
 import os
 import json
 import glob
@@ -5,110 +8,65 @@ import torch
 import random
 import numpy as np
 import nibabel as nib
-from scipy.ndimage import binary_opening
+from joblib import Parallel, delayed
+
+from utils.img_ops import extract_foreground_mask_mri, extract_foreground_mask_ct, random_foreground_crop
 
 
-# RAW_DATA_PATTERN = '/home/eytan/storage/staff/eytankats/data/nako_10k/images_mri_stitched//**/wat.nii.gz'
-# RAW_DATA_PATTERN = '/home/eytan/storage/datasets/TotalSegmentatorV2//**/ct.nii.gz'
-RAW_DATA_PATTERN = '/home/eytan/storage/staff/eytankats/data/total_segmentator_mri//**/mri.nii.gz'
-OUTPUT_DIR = '/home/eytan/storage/staff/eytankats/projects/medssl3d/data/npy128_ts_mri'
+RAW_DATA_PATTERN_MRI = '/home/eytan/storage/staff/eytankats/data/nako_10k/images_mri_stitched//**/wat.nii.gz'
+RAW_DATA_PATTERN_CT = '/home/eytan/storage/datasets/TotalSegmentatorV2//**/ct.nii.gz'
+OUTPUT_DIR = '/home/eytan/storage/staff/eytankats/projects/medssl3d/data/patches_unpaired_nako_tsct'
 DATASET_FILE_NAME = 'dataset.json'
-MAX_PATCH_NUM = 3000
+MAX_PATCH_NUM = 10000
 PATCH_SIZE = (128, 128, 128)
 MIN_FG_RATIO = 0.7
 
-def random_foreground_crop(image, mask, patch_size, min_fg_ratio=0.8, max_tries=100):
-
-    """
-    Randomly crop a 3D patch from an image and mask such that
-    at least `min_fg_ratio` of the patch is foreground in the mask.
-
-    Args:
-        image (ndarray): 3D array of shape (D, H, W).
-        mask (ndarray): 3D binary mask (same shape as image).
-        patch_size (tuple): (d, h, w) size of the patch to crop.
-        min_fg_ratio (float): Minimum ratio of foreground voxels required.
-        max_tries (int): Max attempts before giving up.
-
-    Returns:
-        cropped_img (ndarray)
-    """
-
-    assert image.shape == mask.shape, "Image and mask must have same shape."
-
-    D, H, W = image.shape
-    d, h, w = patch_size
-    if d >= D or h >= H or w >= W:
-        print(f'Warning: Patch size must fit inside image. Patch size: {d}, {h}, {w}. Image size: {D}, {H}, {W}')
-        return None, None, None, None
-
-    best_fg_ratio = 0
-    for _ in range(max_tries):
-
-        # Sample random start indices
-        start_d = np.random.randint(0, D - d + 1)
-        start_h = np.random.randint(0, H - h + 1)
-        start_w = np.random.randint(0, W - w + 1)
-
-        # Crop patch
-        patch_img = image[start_d:start_d + d, start_h:start_h + h, start_w:start_w + w]
-        patch_mask = mask[start_d:start_d + d, start_h:start_h + h, start_w:start_w + w]
-
-        # Check foreground ratio
-        fg_ratio = patch_mask.sum() / patch_mask.size
-        if fg_ratio >= min_fg_ratio:
-            return patch_img, start_d, start_h, start_w
-
-        if fg_ratio > best_fg_ratio:
-            best_fg_ratio = fg_ratio
-            best_patch_img = patch_img
-            best_start_d = start_d
-            best_start_h = start_h
-            best_start_w = start_w
-
-    # If no patch found, return the last one anyway
-    print(f'Warning: Could not find patch with required foreground ratio, saving best attempt with ratio {best_fg_ratio}.' )
-
-    if best_fg_ratio == 0:
-        return None, None, None, None
-    else:
-        return best_patch_img, best_start_d, best_start_h, best_start_w
-
 os.makedirs(OUTPUT_DIR, exist_ok=True)
-dataset = {"training": []}
-img_paths = glob.glob(RAW_DATA_PATTERN)
-for _ in range(MAX_PATCH_NUM):
 
-    img_path = random.choice(img_paths)
+img_paths_mri = glob.glob(RAW_DATA_PATTERN_MRI)
+img_paths_ct = glob.glob(RAW_DATA_PATTERN_CT)
 
-    # load image
-    img_nib = nib.load(img_path)
-    img_np = img_nib.get_fdata(dtype=np.float32)  # copy() is a workaround to prevent negative stride that prevents initializing torch image
 
-    # flip to standartized orientation image
-    img_np = np.flip(img_np, axis=1)
-    img_np = np.flip(img_np, axis=0)
-    img_np = np.ascontiguousarray(img_np)  # avoid negative strides
+def crop_and_save(cnt):
+    patch_found = False
+    while not patch_found:
 
-    # resample to isotropic resolution 1.5x1.5x1.5
-    img_t = torch.from_numpy(img_np[None, None])  # [N, C, D, H, W]
+        if cnt % 2 == 0:
+            img_path = random.choice(img_paths_mri)
+        else:
+            img_path = random.choice(img_paths_ct)
 
-    zooms = img_nib.header.get_zooms()[:3]
-    orig_size = torch.tensor(img_t.shape[-3:])
-    target_size = (orig_size * torch.tensor(zooms) / 1.5).round().int().tolist()
+        # load image
+        img_nib = nib.load(img_path)
+        img_np = img_nib.get_fdata(dtype=np.float32)
 
-    img_t_resampled = torch.nn.functional.interpolate(img_t, size=target_size, mode="trilinear", align_corners=False)
-    img_np_resampled = img_t_resampled.squeeze().numpy()
+        # flip to standartized orientation image
+        img_np = np.flip(img_np, axis=1)
+        img_np = np.flip(img_np, axis=0)
+        img_np = np.ascontiguousarray(img_np)  # avoid negative strides
 
-    # get foreground mask
-    img_np_normalized = (img_np_resampled - np.min(img_np_resampled)) / (np.max(img_np_resampled) - np.min(img_np_resampled))
-    non_zero_mask = img_np_normalized > 0.02
-    non_zero_mask = binary_opening(non_zero_mask, iterations=5) # clean nonzero mask by morphological opening
+        # resample to isotropic resolution
+        img_t = torch.from_numpy(img_np[None, None])  # [N, C, D, H, W]
 
-    # crop patch
-    patch, start_d, start_h, start_w = random_foreground_crop(img_np_resampled, non_zero_mask, patch_size=PATCH_SIZE, min_fg_ratio=MIN_FG_RATIO)
-    if patch is None:
-        continue
+        resolution = torch.rand(1)[0] + 1  # get resolution between 1 and 2
+
+        zooms = img_nib.header.get_zooms()[:3]
+        orig_size = torch.tensor(img_t.shape[-3:])
+        target_size = (orig_size * torch.tensor(zooms) / resolution).round().int().tolist()
+
+        img_t_resampled = torch.nn.functional.interpolate(img_t, size=target_size, mode="trilinear", align_corners=False)
+        img_np_resampled = img_t_resampled.squeeze().numpy()
+
+        # get foreground mask
+        if cnt % 2 == 0:
+            non_zero_mask = extract_foreground_mask_mri(img_t_resampled)
+        else:
+            non_zero_mask = extract_foreground_mask_ct(img_t_resampled)
+
+        # crop patch
+        patch, start_d, start_h, start_w = random_foreground_crop(img_np_resampled, non_zero_mask, patch_size=PATCH_SIZE, min_fg_ratio=MIN_FG_RATIO)
+        if patch is not None:
+            patch_found = True
 
     # import matplotlib.pyplot as plt
     # plt.imshow(patch[:, :, 64], cmap='gray')
@@ -118,8 +76,16 @@ for _ in range(MAX_PATCH_NUM):
     patch_path = os.path.join(OUTPUT_DIR, f'{os.path.basename(os.path.dirname(img_path))}_{start_d}_{start_h}_{start_w}.npy')
     np.save(patch_path, patch)
 
+results = Parallel(n_jobs=10)(
+    delayed(crop_and_save)(patch_idx) for patch_idx in range(MAX_PATCH_NUM)
+)
+
+dataset = {"training": []}
+patches = glob.glob(os.path.join(OUTPUT_DIR, '*.npy'))
+for patch in patches:
+
     # add patch to dataset
-    entry = {"image": os.path.basename(patch_path)}
+    entry = {"image": os.path.basename(patch)}
     dataset["training"].append(entry)
 
 # save dataset file
